d0eefc51a5baf6c6df1978eb955507fe5ca7aeb2
apnagaev <87721833+apnagaev@users.noreply.github.com>
2022-10-31 08:55:01 +0300

    Update inventory.ps1

--- inventory.ps1
+++ inventory.ps1
@@ -230,6 +230,7 @@ $osname=(Get-WmiObject -class Win32_OperatingSystem).Caption
 $osname = $osname -replace 'Майкрософт','Microsoft'
 $osname = $osname -replace 'Профессиональная','Pro'
 $osname = $osname -replace 'Корпоративная','Corp'
+$osname = $osname -replace ' (Registered Trademark)',''
 $allosurl=$allurl+'&qlQuery=objectType="OS"'
 $allosurl
 $allos=Invoke-RestMethod -Uri $allosurl -Headers @{Authorization=("Basic {0}" -f $base64)} -ContentType 'application/json; charset=utf-8'
